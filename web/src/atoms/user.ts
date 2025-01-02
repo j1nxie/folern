3aff2d8db9f98b574188aa204f67a813c9c5ede2
j1nxie <rylieeeeexd@gmail.com>
2025-01-02 09:32:50 +0700

    feat: atoms for fetching user stats / scores

--- web/src/atoms/user.ts
+++ web/src/atoms/user.ts
@@ -1,6 +1,9 @@
 import { atomWithQuery } from "jotai-tanstack-query";
 
 import getUserInfo from "@/lib/api/users/get-info";
+import getUserScores from "@/lib/api/users/get-scores";
+import getUserStats from "@/lib/api/users/get-stats";
+import type { ScoreResponse, StatsResponse } from "@/lib/types/score";
 import type { User } from "@/lib/types/user";
 
 import { isLoggedInAtom } from "./auth";
@@ -8,14 +11,52 @@ import { isLoggedInAtom } from "./auth";
 export const userDataAtom = atomWithQuery(
 	get => ({
 		queryKey: ["user-data"],
-		queryFn: (): Promise<User> | null => {
+		queryFn: async (): Promise<User | null> => {
 			const isLoggedIn = get(isLoggedInAtom);
 
 			if (!isLoggedIn) {
 				return null;
 			}
 
-			return getUserInfo();
+			const result = await getUserInfo();
+
+			return result;
+		},
+		enabled: get(isLoggedInAtom),
+	}),
+);
+
+export const userScoresAtom = atomWithQuery(
+	get => ({
+		queryKey: ["user-scores"],
+		queryFn: async (): Promise<ScoreResponse | null> => {
+			const isLoggedIn = get(isLoggedInAtom);
+
+			if (!isLoggedIn) {
+				return null;
+			}
+
+			const result = await getUserScores();
+
+			return result;
+		},
+		enabled: get(isLoggedInAtom),
+	}),
+);
+
+export const userStatsAtom = atomWithQuery(
+	get => ({
+		queryKey: ["user-stats"],
+		queryFn: async (): Promise<StatsResponse | null> => {
+			const isLoggedIn = get(isLoggedInAtom);
+
+			if (!isLoggedIn) {
+				return null;
+			}
+
+			const result = await getUserStats();
+
+			return result;
 		},
 		enabled: get(isLoggedInAtom),
 	}),
